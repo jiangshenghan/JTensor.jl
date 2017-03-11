be3c98a845f80c79abc1795b0f4ba065653d3e6b
Shenghan <jiangshenghan@gmail.com>
2017-03-11 15:47:41 -0500

    modification

--- example/square_pi_rvb_energy_sl_vumps.jl
+++ example/square_pi_rvb_energy_sl_vumps.jl
@@ -5,29 +5,29 @@ using JTensor
 #julia square_pi_rvb_energy_sl_vumps.jl chi
 
 #pi srvb
-T=[zeros(Complex128,2,3,3,3,3) for i=1:2]
-T[1][1,2,3,3,3]=1
-T[1][2,1,3,3,3]=-1
-T[1][1,3,1,3,3]=-1
-T[1][2,3,2,3,3]=-1
-T[1][1,3,3,1,3]=-1
-T[1][2,3,3,2,3]=-1
-T[1][1,3,3,3,2]=1
-T[1][2,3,3,3,1]=-1
-
-T[2][1,2,3,3,3]=1
-T[2][2,1,3,3,3]=-1
-T[2][1,3,1,3,3]=1
-T[2][2,3,2,3,3]=1
-T[2][1,3,3,1,3]=-1
-T[2][2,3,3,2,3]=-1
-T[2][1,3,3,3,2]=1
-T[2][2,3,3,3,1]=-1
+#T=[zeros(Complex128,2,3,3,3,3) for i=1:2]
+#T[1][1,2,3,3,3]=1
+#T[1][2,1,3,3,3]=-1
+#T[1][1,3,1,3,3]=-1
+#T[1][2,3,2,3,3]=-1
+#T[1][1,3,3,1,3]=-1
+#T[1][2,3,3,2,3]=-1
+#T[1][1,3,3,3,2]=1
+#T[1][2,3,3,3,1]=-1
+#
+#T[2][1,2,3,3,3]=1
+#T[2][2,1,3,3,3]=-1
+#T[2][1,3,1,3,3]=1
+#T[2][2,3,2,3,3]=1
+#T[2][1,3,3,1,3]=-1
+#T[2][2,3,3,2,3]=-1
+#T[2][1,3,3,3,2]=1
+#T[2][2,3,3,3,1]=-1
 
 #pi rvb D=6
-#T=readdlm("/home/jiangsb/code/JTensor.jl/tensor_data/square_pi_flux")
-#T=[T[:,1],T[:,2]]
-#T=[reshape(T[i],2,6,6,6,6) for i=1:2]
+T=readdlm("/home/jiangsb/code/JTensor.jl/tensor_data/square_pi_flux")
+T=[T[:,1],T[:,2]]
+T=[reshape(T[i],2,6,6,6,6) for i=1:2]
 
 
 D=size(T[1],2)
@@ -53,30 +53,31 @@ if D==3
     W=[0 1 0; -1 0 0; 0 0 1]
 end
 #D=6(0+1/2+1) case
-#if D==6
-#    W=zeros(6,6)
-#    W[6,1]=W[3,3]=W[2,5]=W[1,6]=1
-#    W[5,2]=W[4,4]=-1
-#end
+if D==6
+    W=zeros(6,6)
+    W[6,1]=W[3,3]=W[2,5]=W[1,6]=1
+    W[5,2]=W[4,4]=-1
+end
 WW=reshape(jcontract([W,W],[[-1,-3],[-2,-4]]),DD,DD)
 
 #using iTEBD to get initial state
-#Alu=[reshape(jcontract([T[i],conj(T[i])],[[1,-1,2,-3,-5],[1,-2,2,-4,-6]]),DD,DD,DD) for i=1:2]
-#Bu=Cu=[]
-#Flu=Fru=jcontract([eye(Complex128,DD),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
-#for iter=1:2
-#    Bu,Cu,Flu,Fru=sl_mult_mpo_mps(Alu,TTu,chi,Flu,Fru)
-#    Alu=[jcontract([diagm(Cu[3-i]),Bu[i]],[[-1,1],[1,-2,-3]]) for i=1:2]
-#    chi=size(Cu[1],1)
-#    Flu=Fru=jcontract([eye(Complex128,chi),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
-#end
-#Cu=[complex(diagm(Cu[i])) for i=1:2]
-#Aru=[jcontract([Bu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
-#Acu=[jcontract([Alu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
+Alu=[reshape(jcontract([T[i],conj(T[i])],[[1,-1,2,-3,-5],[1,-2,2,-4,-6]]),DD,DD,DD) for i=1:2]
+Bu=Cu=[]
+Flu=Fru=jcontract([eye(Complex128,DD),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
+for iter=1:2
+    Bu,Cu,Flu,Fru=sl_mult_mpo_mps(Alu,TTu,chi,Flu,Fru)
+    Alu=[jcontract([diagm(Cu[3-i]),Bu[i]],[[-1,1],[1,-2,-3]]) for i=1:2]
+    chi=size(Cu[1],1)
+    Flu=Fru=jcontract([eye(Complex128,chi),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
+end
+Cu=[complex(diagm(Cu[i])) for i=1:2]
+Aru=[jcontract([Bu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
+Acu=[jcontract([Alu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
 
-Alu=[permutedims(reshape(qr(rand(Complex128,chi*DD,chi))[1],chi,DD,chi),[1,3,2]) for i=1:2]
-Aru=[permutedims(Alu[i],[2,1,3]) for i=1:2]
-Acu=Cu=[]
+#using radom intial state
+#Alu=[permutedims(reshape(qr(rand(Complex128,chi*DD,chi))[1],chi,DD,chi),[1,3,2]) for i=1:2]
+#Aru=[permutedims(Alu[i],[2,1,3]) for i=1:2]
+#Acu=Cu=[]
 
 Flu=Fru=[reshape(jcontract([eye(Complex128,chi),eye(Complex128,D)],[[-1,-4],[-2,-3]]),chi,DD,chi) for i=1:2]
 err=1.
