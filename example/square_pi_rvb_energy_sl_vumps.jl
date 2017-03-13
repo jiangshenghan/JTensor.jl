4685d39c1b1afe8261075b95b5f8cf4d7191eb20
Shenghan <jiangshenghan@gmail.com>
2017-03-13 14:29:33 -0400

    measurement

--- example/square_pi_rvb_energy_sl_vumps.jl
+++ example/square_pi_rvb_energy_sl_vumps.jl
@@ -69,9 +69,9 @@ Flu=Fru=jcontract([eye(Complex128,size(Alu[1],1)),eye(Complex128,DD)],[[-1,-3],[
 for iter=1:3
     Bu,Cu,Flu,Fru=sl_mult_mpo_mps(Alu,TTu,chi,Flu,Fru)
     Alu=[jcontract([diagm(Cu[3-i]),Bu[i]],[[-1,1],[1,-2,-3]]) for i=1:2]
-    chi=size(Cu[1],1)
-    Flu=Fru=jcontract([eye(Complex128,chi),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
+    Flu=Fru=jcontract([eye(Complex128,size(Cu[1],1)),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
 end
+chi=size(Cu[1],1)
 Cu=[complex(diagm(Cu[i])) for i=1:2]
 Aru=[jcontract([Bu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
 Acu=[jcontract([Alu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
@@ -84,8 +84,6 @@ Acu=[jcontract([Alu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
 Flu=Fru=[reshape(jcontract([eye(Complex128,chi),eye(Complex128,D)],[[-1,-4],[-2,-3]]),chi,DD,chi) for i=1:2]
 err=1.
 
-Gl=Gr=reshape(jcontract([eye(Complex128,chi,chi),eye(Complex128,D)],[[-1,-4],[-2,-3]]),chi,DD,chi)
-
 #VUMPS to obtain energy
 for iter=1:300
     Alu,Aru,Acu,Cu,Flu,Fru,_,err=sl_mult_vumps_par(TTu,chi,Alu,Aru,Acu,Cu,Flu,Fru,e0=err,maxiter=1,ncv=40)
@@ -93,39 +91,8 @@ for iter=1:300
     #lower imps by symmetry
     Ald=[jcontract([Alu[i],WW],[[-1,-2,1],[1,-3]]) for i=1:2]
 
-    leftlm=LinearMap([Gl,Alu[1],TT[1],Ald[1],Alu[2],TT[2],Ald[2]],[[1,2,3],[1,6,4],[2,4,7,5],[8,3,5],[6,-1,9],[7,9,-2,10],[-3,8,10]],1)
-    leig_res=eigs(leftlm,nev=1,v0=Gl[:],ncv=20,tol=1e-8)
-    λl,Gl=leig_res
-    λl=λl[1]
-    Gl=reshape(Gl,chi,DD,chi)
-
-    rightlm=LinearMap([Gr,Alu[2],TT[2],Ald[2],Alu[1],TT[1],Ald[1]],[[1,2,3],[6,1,4],[7,4,2,5],[3,8,5],[-1,6,9],[-2,9,7,10],[8,-3,10]],1)
-    reig_res=eigs(rightlm,nev=1,v0=Gr[:],ncv=20,tol=1e-8)
-    λr,Gr=reig_res
-    λr=λr[1]
-    Gr=reshape(Gr,chi,DD,chi)
-
-    @printf("eig info: \n λl=%f+i%f \n λr=%f+i%f \n lniter=%d, lnmult=%d \n rniter=%d, rnmult=%d \n",real(λl),imag(λl),real(λr),imag(λr),leig_res[4],leig_res[5],reig_res[4],reig_res[5])
-
-    wf_norm=λl*jcontract([Gl,Gr],[[1,2,3],[1,2,3]])
-    EL=jcontract([Gl,Alu[1],THl,Ald[1]],[[1,2,3],[1,-1,4],[2,4,-2,5],[-3,3,5]])
-    ER=jcontract([Gr,Alu[2],THr,Ald[2]],[[1,2,3],[-1,1,4],[-2,4,2,5],[3,-3,5]])
-    energy=jcontract([EL,ER],[[1,2,3],[1,2,3]])/wf_norm
-
-    @printf("iter=%d\n wf_norm=%f+i%f\n energy=%.16f+i%e\n\n",iter,real(wf_norm),imag(wf_norm),real(energy),imag(energy))
-     flush(STDOUT)
-
-     #if iter%10==0
-     #    Hlu=Hru=jcontract([eye(Complex128,chi),eye(Complex128,DD)],[[-1,-3],[-2,-4]])
-     #    Bu,Cu=sl_mult_mpo_mps(Alu,TTu,chi,Hlu,Hru)
-     #    Alu=[jcontract([diagm(Cu[3-i]),Bu[i]],[[-1,1],[1,-2,-3]]) for i=1:2]
-     #    chi=size(Cu[1],1)
-     #    Cu=[complex(diagm(Cu[i])) for i=1:2]
-     #    Aru=[jcontract([Bu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
-     #    Acu=[jcontract([Alu[i],Cu[i]],[[-1,1,-3],[1,-2]]) for i=1:2]
-     #    Flu=Fru=[reshape(jcontract([eye(Complex128,chi),eye(Complex128,D)],[[-1,-4],[-2,-3]]),chi,DD,chi) for i=1:2]
-     #    Gl=Gr=reshape(jcontract([eye(Complex128,chi,chi),eye(Complex128,D)],[[-1,-4],[-2,-3]]),chi,DD,chi)
-     #end
+    @printf("iter=%d\n",iter)
+    square_heisenberg(Aru,Ald,T)
 end
 
 
--- src/JTensor.jl
+++ src/JTensor.jl
@@ -4,6 +4,7 @@ module JTensor
 export jcontract, LinearMap, polardecomp 
 export sl_one_vumps, sl_two_vumps, sl_mult_vumps_par, dl_one_vumps, dl_two_vumps, dl_mult_vumps_seq
 export sl_mult_mpo_mps, dl_mult_mpo_mps
+export square_heisenberg
 
 include("jcontract.jl")
 include("linearmap.jl")
@@ -18,5 +19,7 @@ include("vumps/dl_mult_vumps_seq.jl")
 
 include("itebd/sl_mult_mpo_mps.jl")
 include("itebd/dl_mult_mpo_mps.jl")
+
+include("measurement/heisenberg_energy.jl")
 
 end
