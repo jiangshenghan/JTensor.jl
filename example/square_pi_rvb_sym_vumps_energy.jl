c407f91bc47d45d38f9c9c3a5c237669a57d68e7
Shenghan <jiangshenghan@gmail.com>
2017-03-16 12:02:49 -0400

    modification

--- example/square_pi_rvb_sym_vumps_energy.jl
+++ example/square_pi_rvb_sym_vumps_energy.jl
@@ -2,7 +2,8 @@ include("../src/JTensor.jl")
 using JTensor
 
 #chi_spin=[0,0,1,1,1.5,1.5]
-chi_spin=[0,0,0,0,0,1,1,1,1,1.5,1.5,1.5,1.5]
+#chi_spin=[0,0,0,0,0,1,1,1,1,1.5,1.5,1.5,1.5]
+chi_spin=[0,0,0,0.5,0.5,0.5,0.5]
 chi=Int(sum(x->2x+1,chi_spin))
 maxiter=100
 println("chi=",chi)
@@ -13,32 +14,32 @@ flush(STDOUT)
 
 
 #pi srvb
-#T=[zeros(Complex128,2,3,3,3,3) for i=1:2]
-#T[1][1,2,3,3,3]=1
-#T[1][2,1,3,3,3]=-1
-#T[1][1,3,1,3,3]=-1
-#T[1][2,3,2,3,3]=-1
-#T[1][1,3,3,1,3]=-1
-#T[1][2,3,3,2,3]=-1
-#T[1][1,3,3,3,2]=1
-#T[1][2,3,3,3,1]=-1
-#
-#T[2][1,2,3,3,3]=1
-#T[2][2,1,3,3,3]=-1
-#T[2][1,3,1,3,3]=1
-#T[2][2,3,2,3,3]=1
-#T[2][1,3,3,1,3]=-1
-#T[2][2,3,3,2,3]=-1
-#T[2][1,3,3,3,2]=1
-#T[2][2,3,3,3,1]=-1
-#
-#virt_spin=[0.5,0]
+T=[zeros(Complex128,2,3,3,3,3) for i=1:2]
+T[1][1,2,3,3,3]=1
+T[1][2,1,3,3,3]=-1
+T[1][1,3,1,3,3]=-1
+T[1][2,3,2,3,3]=-1
+T[1][1,3,3,1,3]=-1
+T[1][2,3,3,2,3]=-1
+T[1][1,3,3,3,2]=1
+T[1][2,3,3,3,1]=-1
+
+T[2][1,2,3,3,3]=1
+T[2][2,1,3,3,3]=-1
+T[2][1,3,1,3,3]=1
+T[2][2,3,2,3,3]=1
+T[2][1,3,3,1,3]=-1
+T[2][2,3,3,2,3]=-1
+T[2][1,3,3,3,2]=1
+T[2][2,3,3,3,1]=-1
+
+virt_spin=[0.5,0]
 
 #pi rvb D=6
-T=readdlm("/home/jiangsb/code/JTensor.jl/tensor_data/square_pi_flux")
-T=[T[:,1],T[:,2]]
-T=[reshape(T[i],2,6,6,6,6) for i=1:2]
-virt_spin=[0,0.5,1]
+#T=readdlm("/home/jiangsb/code/JTensor.jl/tensor_data/square_pi_flux")
+#T=[T[:,1],T[:,2]]
+#T=[reshape(T[i],2,6,6,6,6) for i=1:2]
+#virt_spin=[0,0.5,1]
 
 
 #initialize
@@ -47,8 +48,9 @@ DD=D^2
 TTu=[permutedims(reshape(jcontract([T[i],conj(T[i])],[[1,-1,-3,-5,-7],[1,-2,-4,-6,-8]]),DD,DD,DD,DD),[1,3,2,4]) for i=1:2]
 
 #spin symmetric subspace
-M=spin_sym_space([chi_spin,chi_spin,virt_spin,virt_spin],[1,-1,1,-1])
-M=reshape(M,chi,chi,DD,size(M)[end])
+MA=spin_sym_space([chi_spin,chi_spin,virt_spin,virt_spin],[1,-1,1,-1])
+MA=reshape(MA,chi,chi,DD,size(MA)[end])
+MC=spin_sym_space([chi_spin,chi_spin],[1,-1])
 
 #symmetry transformation
 #D=3 case
@@ -64,17 +66,23 @@ end
 WW=reshape(jcontract([W,W],[[-1,-3],[-2,-4]]),DD,DD)
 
 Alu=rand(Complex128,chi,chi,DD)
-Alu=sym_tensor_proj(Alu,M)
 Alu=[Alu,Alu]
 Aru=Alu
+Acu=Alu
+Cu=rand(Complex128,chi,chi)
+Cu=[Cu,Cu]
 err=1.
 
 for iter=1:maxiter
-    Alu,Aru,_,_,_,_,_,err=sl_mult_vumps_par(TTu,chi,Alu,Aru,e0=err/10,maxiter=1,ncv=20)
-    Alu[1]=sym_tensor_proj(Alu[1],M)
-    Aru[1]=sym_tensor_proj(Aru[1],M)
+    Alu,Aru,Acu,Cu,_,_,_,err=sl_mult_vumps_par(TTu,chi,Alu,Aru,Acu,Cu,e0=err/10,maxiter=1,ncv=20)
+    Alu[1]=sym_tensor_proj(Alu[1],MA)
+    Aru[1]=sym_tensor_proj(Aru[1],MA)
+    Acu[1]=sym_tensor_proj(Acu[1],MA)
+    Cu[1]=sym_tensor_proj(Cu[1],MC)
     Alu=[Alu[1],Alu[1]]
     Aru=[Aru[1],Aru[1]]
+    Acu=[Acu[1],Acu[1]]
+    Cu=[Cu[1],Cu[1]]
 
     #lower imps by symmetry
     Ald=[jcontract([Alu[i],WW],[[-1,-2,1],[1,-3]]) for i=1:2]
