4a31a334213307305ef63c1608525847646c3b52
shenghan <jiangshenghan@gmail.com>
2017-03-16 11:06:48 -0400

    modification

--- example/square_pi_rvb_sym_vumps_energy.jl
+++ example/square_pi_rvb_sym_vumps_energy.jl
@@ -1,7 +1,8 @@
 include("../src/JTensor.jl")
 using JTensor
 
-chi_spin=[0,0,1,1,1.5,1.5]
+#chi_spin=[0,0,1,1,1.5,1.5]
+chi_spin=[0,0,0,0,0,1,1,1,1,1.5,1.5,1.5,1.5]
 chi=Int(sum(x->2x+1,chi_spin))
 maxiter=100
 println("chi=",chi)
@@ -11,16 +12,38 @@ println()
 flush(STDOUT)
 
 
+#pi srvb
+#T=[zeros(Complex128,2,3,3,3,3) for i=1:2]
+#T[1][1,2,3,3,3]=1
+#T[1][2,1,3,3,3]=-1
+#T[1][1,3,1,3,3]=-1
+#T[1][2,3,2,3,3]=-1
+#T[1][1,3,3,1,3]=-1
+#T[1][2,3,3,2,3]=-1
+#T[1][1,3,3,3,2]=1
+#T[1][2,3,3,3,1]=-1
+#
+#T[2][1,2,3,3,3]=1
+#T[2][2,1,3,3,3]=-1
+#T[2][1,3,1,3,3]=1
+#T[2][2,3,2,3,3]=1
+#T[2][1,3,3,1,3]=-1
+#T[2][2,3,3,2,3]=-1
+#T[2][1,3,3,3,2]=1
+#T[2][2,3,3,3,1]=-1
+#
+#virt_spin=[0.5,0]
+
 #pi rvb D=6
 T=readdlm("/home/jiangsb/code/JTensor.jl/tensor_data/square_pi_flux")
 T=[T[:,1],T[:,2]]
 T=[reshape(T[i],2,6,6,6,6) for i=1:2]
+virt_spin=[0,0.5,1]
 
 
 #initialize
 D=size(T[1],2)
 DD=D^2
-virt_spin=[0,0.5,1]
 TTu=[permutedims(reshape(jcontract([T[i],conj(T[i])],[[1,-1,-3,-5,-7],[1,-2,-4,-6,-8]]),DD,DD,DD,DD),[1,3,2,4]) for i=1:2]
 
 #spin symmetric subspace
@@ -41,15 +64,15 @@ end
 WW=reshape(jcontract([W,W],[[-1,-3],[-2,-4]]),DD,DD)
 
 Alu=rand(Complex128,chi,chi,DD)
-Alu=spin_tensor_proj(Alu,M)
+Alu=sym_tensor_proj(Alu,M)
 Alu=[Alu,Alu]
 Aru=Alu
 err=1.
 
 for iter=1:maxiter
     Alu,Aru,_,_,_,_,_,err=sl_mult_vumps_par(TTu,chi,Alu,Aru,e0=err/10,maxiter=1,ncv=20)
-    Alu[1]=spin_tensor_proj(Alu[1],M)
-    Aru[1]=spin_tensor_proj(Aru[1],M)
+    Alu[1]=sym_tensor_proj(Alu[1],M)
+    Aru[1]=sym_tensor_proj(Aru[1],M)
     Alu=[Alu[1],Alu[1]]
     Aru=[Aru[1],Aru[1]]
 
