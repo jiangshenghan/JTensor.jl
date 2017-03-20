63beedeecc262ce837eb8fb0b054f964ca1860f2
Shenghan <jiangshenghan@gmail.com>
2017-03-20 15:14:33 -0400

    modification

--- src/matdecomp.jl
+++ src/matdecomp.jl
@@ -36,36 +36,53 @@ function svd_spin_sym_tensor(T,left_legs,spin_reps,arrows)
     lmax_spin=sum(i->max(spin_reps[left_legs[i]]...),rl)
     rmax_spin=sum(i->max(spin_reps[right_legs[i]]...),rr)
     max_spin=min(lmax_spin,rmax_spin)
-    @show rl,rr,leg_order
-    @show lmax_spin,rmax_spin,max_spin
-
 
     Us=[]
     Ss=[]
     Vts=[]
     spins=[]
 
-    for si=0:0.5:max_spin
-        ML=spin_singlet_space_from_cg([spin_reps[left_legs]...,[si]],[arrows[left_legs]...,-1])*sqrt(2*si+1)
-        MR=spin_singlet_space_from_cg([spin_reps[right_legs]...,[si]],[arrows[right_legs]...,1])*sqrt(2*si+1)
+    PL=zeros(size(T)[left_legs]...,size(T)[left_legs]...)
+    PR=zeros(size(T)[right_legs]...,size(T)[right_legs]...)
+
+    for si=0:0.5:rmax_spin
+        ML=spin_singlet_space_from_cg([spin_reps[left_legs]...,[si]],[-arrows[left_legs]...,-1])*sqrt(2*si+1)
+        MR=spin_singlet_space_from_cg([spin_reps[right_legs]...,[si]],[-arrows[right_legs]...,1])*sqrt(2*si+1)
         if ML==[] || MR==[] continue end
 
-        @show si
-        @show reshape(jcontract([ML,conj(ML)],[[rl...,-1,-2],[rl...,-3,-4]]),size(ML)[end]*Int(2*si+1),size(ML)[end]*Int(2*si+1))
-        @show reshape(jcontract([MR,conj(MR)],[[rr...,-1,-2],[rr...,-3,-4]]),size(MR)[end]*Int(2*si+1),size(MR)[end]*Int(2*si+1))
-
         Tsi=jcontract([ML[[Colon() for i=rl]...,1,:],T,MR[[Colon() for i=rr]...,1,:]],[[rl...,-1],leg_order,[(length(left_legs)+rr)...,-2]])
-        @show Tsi
-
         svd_res=svdfact(Tsi)
-        Usi=reshape(jcontract([svd_res[:U],eye(Int(2*si+1))],[[-1,-3],[-2,-4]]),size(svd_res[:U],1),Int(2*si+1),size(svd_res[:U],2)*Int(2*si+1))
-        Vtsi=reshape(jcontract([svd_res[:Vt],eye(Int(2*si+1))],[[-1,-3],[-2,-4]]),size(svd_res[:Vt],1)*Int(2*si+1),size(svd_res[:Vt],2),Int(2*si+1))
+        Usi=reshape(jcontract([eye(Int(2*si+1)),svd_res[:U]],[[-1,-3],[-2,-4]]),Int(2*si+1),size(svd_res[:U],1),Int(2*si+1)*size(svd_res[:U],2))
+        Vtsi=reshape(jcontract([eye(Int(2*si+1)),svd_res[:Vt]],[[-1,-3],[-2,-4]]),Int(2*si+1)*size(svd_res[:Vt],1),Int(2*si+1),size(svd_res[:Vt],2))
 
         push!(Ss,repeat(svd_res[:S],inner=Int(2*si+1)))
-        push!(Us,jcontract([conj(ML),Usi],[[-rl...,1,2],[2,1,-rl[end]-1]]))
-        push!(Vts,jcontract([Vtsi,conj(MR)],[[-1,2,1],[(-rr-1)...,1,2]]))
+        push!(Us,jcontract([conj(ML),Usi],[[-rl...,1,2],[1,2,-rl[end]-1]]))
+        push!(Vts,jcontract([Vtsi,conj(MR)],[[-1,1,2],[(-rr-1)...,1,2]]))
         push!(spins,si)
+
+        PL=jcontract([conj(ML),ML],[[-rl...,1,2],[(-rl-rl[end])...,1,2]])
+        PR=jcontract([conj(MR),MR],[[-rr...,1,2],[(-rr-rr[end])...,1,2]])
+        Tp=jcontract([PL,T,PR],[[-rl...,rl...],[1:ndims(T)...],[(-rr-rl[end])...,(rr+rl[end])...]])
+        USVi=jcontract([Us[end],diagm(Ss[end]),Vts[end]],[[-rl...,1],[1,2],[2,(-rr-rl[end])...]])
+
     end
+
+    #test svd
+    #U=zeros(size(T)[left_legs]...,sum(i->size(Us[i])[end],1:length(Us)))
+    #Vt=zeros(sum(i->size(Vts[i],1),1:length(Vts)),size(T)[right_legs]...)
+    #ind=1
+    #for i=1:length(Us)
+    #    U[[Colon() for k=rl]...,ind:ind+size(Us[i])[end]-1]=Us[i] 
+    #    ind+=size(Us[i])[end]
+    #end
+    #ind=1
+    #for i=1:length(Vts)
+    #    Vt[ind:ind+size(Vts[i],1)-1,[Colon() for k=rr]...]=Vts[i] 
+    #    ind+=size(Vts[i],1)
+    #end
+    #S=vcat([Ss[i] for i=1:length(Ss)]...)
+    #@show size(jcontract([U,diagm(S),Vt],[[-rl...,1],[1,2],[2,(-rr-rl[end])...]]))
+    #@show vecnorm(jcontract([U,diagm(S),Vt],[[-rl...,1],[1,2],[2,(-rr-rl[end])...]])-permutedims(T,[left_legs...,right_legs...]))
 
     return Us,Ss,Vts,spins
 end
