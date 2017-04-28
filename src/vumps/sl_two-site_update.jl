a663c2a9bb609fb0de17f103dbe3cdd984a6be97
Shenghan <jiangshenghan@gmail.com>
2017-04-28 10:24:34 -0400

    spin_sym_incD

--- src/vumps/sl_two-site_update.jl
+++ src/vumps/sl_two-site_update.jl
@@ -16,3 +16,23 @@ return A2c
 function mag_trans_A2c(T,Fl,Fr,Al,Ac,Jc)
     return jcontract([Fl,Jc,Al,T,Jc,Ac,T,Jc,Fr],[[1,2,-1],[1,3],[3,5,4],[2,6,4,-3],[5,7],[7,9,8],[6,10,8,-4],[9,11],[11,10,-2]])
 end
+
+
+"""
+For spin symmetric MPS, given Al,Ar,A2c, obtain updated Al',Ar',C' with increase bond dimension
+
+   ----A2c----                           ---Al/r--
+   |  /   \  |  = --U--S--Vt--,  where   |  |       =0
+   --Nl   Nr--                           ---Nl/r--
+      |   |
+
+updated:
+Al'= Al (Nl^*)U    Ar'= Ar       0    C'= C 0
+     0  0               Vt(Nr^*) 0        0 0
+
+return Al',Ar',C'
+"""
+function spin_sym_two-site_incD(Al,Ar,A2c)
+    #obtain null space
+    Nl,nl_spin_reps=spin_sym_tensor_nullspace(Al)
+end
