4d461a7e5d92bafebe939e8cb2678d2d8daffafe
jiangshenghan <jiangshenghan@gmail.com>
2018-01-03 13:48:00 -0800

    tebd

--- tebd_example/qubit_corr.jl
+++ tebd_example/qubit_corr.jl
@@ -4,8 +4,8 @@ using JTensor
 
 L=40
 dp=2
-δt=0.02
-tf=500
+δt=0.01
+tf=1000
 @show L,δt,tf
 
 I=[1 0; 0 1]
@@ -35,14 +35,14 @@ Ue=[]
 for k=1:div(L-1,2)
     j=2k
     Heig=eigfact(Hmat[j])
-    push!(Ue,Heig[:vectors]*diagm(exp.(im*δt*Heig[:values]))*Heig[:vectors]')
+    push!(Ue,Heig[:vectors]*diagm(exp.(-im*δt*Heig[:values]))*Heig[:vectors]')
     Ue[k]=reshape(transpose(Ue[k]),dp,dp,dp,dp)
 end
 Uo=[]
 for k=1:div(L,2)
     j=2k-1
     Heig=eigfact(Hmat[j])
-    push!(Uo,Heig[:vectors]*diagm(exp.(im*0.5δt*Heig[:values]))*Heig[:vectors]')
+    push!(Uo,Heig[:vectors]*diagm(exp.(-im*0.5δt*Heig[:values]))*Heig[:vectors]')
     Uo[k]=reshape(transpose(Uo[k]),dp,dp,dp,dp)
 end
 
@@ -59,17 +59,32 @@ for j=1:L-1 push!(B,ones(1,1)) end
 #tebd
 t=0
 while t<tf
+    #The measurement assumes canonical form for MPS
     bdry_tens=jcontract([A[1],B[1]],[[-1,1,-3],[1,-2]])
     bdry_corr=jcontract([bdry_tens,Z,conj(bdry_tens)],[[1,2,3],[3,4],[1,2,4]])/norm(bdry_tens[:])
     bulk_ind=div(L,2)
     bulk_tens=jcontract([B[bulk_ind-1],A[bulk_ind],B[bulk_ind]],[[-1,1],[1,2,-3],[2,-2]])
     bulk_corr=-jcontract([bulk_tens,Z,conj(bulk_tens)],[[1,2,3],[3,4],[1,2,4]])/norm(bulk_tens[:])
 
-    #meaure <Z_1 Z_L>
-    ZZ_corr=jcontract([A[1],Z,conj(A[1]),B[1],conj(B[1])],[[1,4,2],[2,3],[1,5,3],[4,-1],[5,-2]])
-    for j=2:L-1
-        ZZ_corr=jcontract([ZZ_corr,A[j],conj(A[j]),B[j],conj(B[j])],[[1,2],[1,4,3],[2,5,3],[4,-1],[5,-2]])
-    end
+    #obtain correlator for integer val of t
+    #if abs(t-round(t))<1e-7
+    #    #norm of wavefunction, should be one if in canonical form
+    #    wf_norm=jcontract([A[1],conj(A[1]),B[1],conj(B[1])],[[1,3,2],[1,4,2],[3,-1],[4,-2]])
+    #    for j=2:L-1
+    #        wf_norm=jcontract([wf_norm,A[j],conj(A[j]),B[j],conj(B[j])],[[1,2],[1,4,3],[2,5,3],[4,-1],[5,-2]])
+    #    end
+    #    wf_norm=jcontract([wf_norm,A[L],conj(A[L])],[[1,2],[1,4,3],[2,4,3]])
+
+    #    #meaure <Z_1 Z_L>
+    #    ZZ_corr=jcontract([A[1],Z,conj(A[1]),B[1],conj(B[1])],[[1,4,2],[2,3],[1,5,3],[4,-1],[5,-2]])
+    #    for j=2:L-1
+    #        ZZ_corr=jcontract([ZZ_corr,A[j],conj(A[j]),B[j],conj(B[j])],[[1,2],[1,4,3],[2,5,3],[4,-1],[5,-2]])
+    #    end
+    #    ZZ_corr=jcontract([ZZ_corr,A[L],Z,conj(A[L])],[[1,2],[1,5,3],[3,4],[2,5,4]])
+    #    ZZ_corr=ZZ_corr/wf_norm
+
+    #    @show t,wf_norm,ZZ_corr
+    #end
     
     @show t,bdry_corr,bulk_corr
     A,B=tebd_even_odd_one_step(A,B,Ue,Uo;eps=1e-6)
